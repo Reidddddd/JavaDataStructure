779d5c102cbda9615d73f162d67f86efa4ae37a5
Reid <reidddchan@outlook.com>
2016-11-12 01:04:22 +0800

    JAVA-00030 TCRN-22: InetAddress

---
+++ src/train/tcrn/c22/InetAddressTest.java
@@ -0,0 +1,17 @@
+package train.tcrn.c22;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressTest {
+  public static void main(String[] args) throws UnknownHostException {
+    InetAddress address = InetAddress.getLocalHost();
+    System.out.println(address);
+    address = InetAddress.getByName("www.momo.com");
+    System.out.println(address);
+    InetAddress[] sw = InetAddress.getAllByName("www.nba.com");
+    for (int i = 0; i < sw.length; i++) {
+      System.out.println(sw[i]);
+    }
+  }
+}
