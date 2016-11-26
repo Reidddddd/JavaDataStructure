43d753c37974f0cbb5451a9c130260cb9e32a5fb
Reid <reidddchan@outlook.com>
2016-11-26 21:36:34 +0800

    JAVA-00042 TCRN-17: System

---
+++ src/train/tcrn/c17/Elapsed.java
@@ -0,0 +1,12 @@
+package train.tcrn.c17;
+
+public class Elapsed {
+  public static void main(String[] args) {
+    long start, end;
+    System.out.println("Timing a for loop from 0 to 100,000,000");
+    start = System.currentTimeMillis();
+    for (long i = 0; i < 100000000L; i++);
+    end = System.currentTimeMillis();
+    System.out.println("Elapsed time: " + (end - start));
+  }
+}
---
+++ src/train/tcrn/c17/ShowUserDir.java
@@ -0,0 +1,7 @@
+package train.tcrn.c17;
+
+public class ShowUserDir {
+  public static void main(String[] args) {
+    System.out.println(System.getProperty("user.dir"));
+  }
+}
