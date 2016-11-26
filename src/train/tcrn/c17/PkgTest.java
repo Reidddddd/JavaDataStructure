31ec482b41f4410242f2f65dab7f6353c897e770
Reid <reidddchan@outlook.com>
2016-11-26 21:43:02 +0800

    JAVA-00047 TCRN-17: Package

---
+++ src/train/tcrn/c17/PkgTest.java
@@ -0,0 +1,11 @@
+package train.tcrn.c17;
+
+public class PkgTest {
+  public static void main(String[] args) {
+    Package pkgs[] = Package.getPackages();
+    for (int i = 0; i < pkgs.length; i++) {
+      System.out.println(pkgs[i].getName() + " " + pkgs[i].getImplementationTitle() + " "
+          + pkgs[i].getImplementationVendor() + " " + pkgs[i].getImplementationVersion());
+    }
+  }
+}
