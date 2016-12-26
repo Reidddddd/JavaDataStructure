c85b7322e000d4422986dfdff52668fdea5b409c
Reid <reidddchan@outlook.com>
2016-12-27 00:21:35 +0800

    JAVA-00060 TCRN-19: Date

---
+++ src/train/tcrn/c19/DateDemo.java
@@ -0,0 +1,12 @@
+package train.tcrn.c19;
+
+import java.util.Date;
+
+public class DateDemo {
+  public static void main(String[] args) {
+    Date date = new Date();
+    System.out.println(date);
+    long msec = date.getTime();
+    System.out.println("Milliseconds since Jan. 1, 1970, GMT = " + msec);
+  }
+}
